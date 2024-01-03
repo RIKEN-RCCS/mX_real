7d5342ddbee81d796b84f2d6f03b47bfd6535edd
toshiyuki-imamura <imamura.toshiyuki@riken.jp>
2024-01-03 20:39:04 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 OMP_NUM_THREADS=`lscpu | awk '/^Core\(s/{Cores=$4}/^Socket\(s/{Socket=$2}END{print Cores*Socket}'`
 HOST=`hostname`-${OMP_NUM_THREADS}th
 G=`expr $OMP_NUM_THREADS - 1`
 
 for cxx in g++ icpx; do
-\rm sample.exe
-CXX=$cxx make sample.exe
-cp sample.exe sample.exe-$cxx
+	\rm sample.exe
+	CXX=$cxx make sample.exe
+	cp sample.exe sample.exe-$cxx
 done
 
 for cxx in g++ icpx; do
-OPT=$cxx
-i=0
-for MX in SW PA DW QTW TW QQW QW; do
-numactl --physcpubind=0-$G ./sample.exe-$cxx $i < IN | tee log-$MX-$OPT-$HOST
-i=`expr $i + 1`
-done
+	OPT=$cxx
+	i=0
+	for MX in SW PA DW QTW TW QQW QW; do
+		numactl --physcpubind=0-$G ./sample.exe-$cxx $i < IN | tee log-$MX-$OPT-$HOST
+		i=`expr $i + 1`
+	done
 done
 
