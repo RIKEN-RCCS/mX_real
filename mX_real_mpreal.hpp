46c7ae3421d5fd034fd047d5d23591a0e4c91716
toshiyuki-imamura <imamura.toshiyuki@riken.jp>
2024-09-12 17:21:42 +0900

    Update mX_real_mpreal.hpp

--- mX_real_mpreal.hpp
+++ mX_real_mpreal.hpp
@@ -12,19 +12,19 @@ namespace mX_real {
 namespace QxW {
 
   template <> struct fp_const<mpfr::mpreal> {
-    static INLINE auto  zero() NOEXCEPT { return mpfr::mpreal(0.); }
-    static INLINE auto  one() NOEXCEPT { return mpfr::mpreal(1.); }
-    static INLINE auto  two() NOEXCEPT { return mpfr::mpreal(2.); }
-    static INLINE auto  nhalf() NOEXCEPT { return mpfr::mpreal(.5); }
-    static INLINE auto  threehalves() NOEXCEPT { return mpfr::mpreal(1.5); }
-    static INLINE auto  near_pow2(const mpfr::mpreal& x, mp_rnd_t r = mpfr::mpreal::get_default_rnd())
+    static INLINE auto const zero() NOEXCEPT { return mpfr::mpreal(0.); }
+    static INLINE auto const one() NOEXCEPT { return mpfr::mpreal(1.); }
+    static INLINE auto const two() NOEXCEPT { return mpfr::mpreal(2.); }
+    static INLINE auto const nhalf() NOEXCEPT { return mpfr::mpreal(.5); }
+    static INLINE auto const threehalves() NOEXCEPT { return mpfr::mpreal(1.5); }
+    static INLINE auto const near_pow2(const mpfr::mpreal& x, mp_rnd_t r = mpfr::mpreal::get_default_rnd())
     {
       mpfr::mpreal y(1, x.getPrecision());
       if(!iszero(x)) y = floor(log2(abs(x,r),r));
       return pow(mpfr::mpreal(2,x.getPrecision()),y,r);
     }
-    static INLINE auto exponent( mpfr::mpreal const& a ) NOEXCEPT { return near_pow2( a ); }
-    static INLINE auto exponenti( mpfr::mpreal const& a ) NOEXCEPT { return one()/exponent( a ); }
+    static INLINE auto const exponent( mpfr::mpreal const& a ) NOEXCEPT { return near_pow2( a ); }
+    static INLINE auto const exponenti( mpfr::mpreal const& a ) NOEXCEPT { return one()/exponent( a ); }
   };
 
 }
@@ -34,31 +34,31 @@ namespace QxW {
     static bool   constexpr value = true;
     using _fp_ = QxW::fp_const<mpfr::mpreal>;
 
-    static INLINE auto  zero()       NOEXCEPT { return _fp_::zero(); }
-    static INLINE auto  one()        NOEXCEPT { return _fp_::one(); }
-    static INLINE auto  two()        NOEXCEPT { return _fp_::two(); }
-    static INLINE auto  nhalf()      NOEXCEPT { return _fp_::nhalf(); }
-    static INLINE auto  threehalves() NOEXCEPT { return _fp_::threehalves(); }
+    static INLINE auto const zero()       NOEXCEPT { return _fp_::zero(); }
+    static INLINE auto const one()        NOEXCEPT { return _fp_::one(); }
+    static INLINE auto const two()        NOEXCEPT { return _fp_::two(); }
+    static INLINE auto const nhalf()      NOEXCEPT { return _fp_::nhalf(); }
+    static INLINE auto const threehalves() NOEXCEPT { return _fp_::threehalves(); }
 
-    static INLINE auto  epsilon()    NOEXCEPT { return mpfr::machine_epsilon(); }
-    static INLINE auto  epsiloni()   NOEXCEPT { return one()/epsilon(); }
-    static INLINE auto  nan()        NOEXCEPT { return mpfr::mpreal().setNan(); }
-    static INLINE auto  inf()        NOEXCEPT { return mpfr::const_infinity(); }
-    static INLINE auto  denorm_min() NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::denorm_min(); }
-    static INLINE auto  min()        NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::min(); }
-    static INLINE auto  max()        NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::max(); }
+    static INLINE auto const epsilon()    NOEXCEPT { return mpfr::machine_epsilon(); }
+    static INLINE auto const epsiloni()   NOEXCEPT { return one()/epsilon(); }
+    static INLINE auto const nan()        NOEXCEPT { return mpfr::mpreal().setNan(); }
+    static INLINE auto const inf()        NOEXCEPT { return mpfr::const_infinity(); }
+    static INLINE auto const denorm_min() NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::denorm_min(); }
+    static INLINE auto const min()        NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::min(); }
+    static INLINE auto const max()        NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::max(); }
 
-    static INLINE auto  digits()       NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::digits(); }
-    static INLINE auto  digits10()     NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::digits10(); }
-    static INLINE auto  max_digits10() NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::max_digits10(); }
+    static INLINE auto const digits()       NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::digits(); }
+    static INLINE auto const digits10()     NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::digits10(); }
+    static INLINE auto const max_digits10() NOEXCEPT { return std::numeric_limits<mpfr::mpreal>::max_digits10(); }
 
-    static INLINE auto isinf ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::isinf( a ); }
-    static INLINE auto isnan ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::isnan( a ); }
-    static INLINE auto copysign ( mpfr::mpreal const& a, mpfr::mpreal const& b ) NOEXCEPT { return mpfr::copysign( a, b ); }
-    static INLINE auto signbit ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::signbit( a ); }
-    static INLINE auto is_zero ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::iszero( a ); }
-    static INLINE auto is_positive ( mpfr::mpreal const& a ) NOEXCEPT { return (zero() < a); }
-    static INLINE auto is_negative ( mpfr::mpreal const& a ) NOEXCEPT { return (zero() > a); }
+    static INLINE auto const isinf ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::isinf( a ); }
+    static INLINE auto const isnan ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::isnan( a ); }
+    static INLINE auto const copysign ( mpfr::mpreal const& a, mpfr::mpreal const& b ) NOEXCEPT { return mpfr::copysign( a, b ); }
+    static INLINE auto const signbit ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::signbit( a ); }
+    static INLINE auto const is_zero ( mpfr::mpreal const& a ) NOEXCEPT { return mpfr::iszero( a ); }
+    static INLINE auto const is_positive ( mpfr::mpreal const& a ) NOEXCEPT { return (zero() < a); }
+    static INLINE auto const is_negative ( mpfr::mpreal const& a ) NOEXCEPT { return (zero() > a); }
   };
 #undef	STATIC_VAR
 #undef	STATIC_INIT
@@ -68,13 +68,13 @@ namespace QxW {
   // templated fuction style
   // zero<mpfr::mpreal>()
   //
-  template < typename T, T_mpreal(T) > static INLINE auto  zero()    NOEXCEPT { return fp<mpfr::mpreal>::zero(); }
-  template < typename T, T_mpreal(T) > static INLINE auto  one()     NOEXCEPT { return fp<mpfr::mpreal>::one(); }
-  template < typename T, T_mpreal(T) > static INLINE auto  two()     NOEXCEPT { return fp<mpfr::mpreal>::two(); }
-  template < typename T, T_mpreal(T) > static INLINE auto  nhalf()   NOEXCEPT { return fp<mpfr::mpreal>::nhalf(); }
-  template < typename T, T_mpreal(T) > static INLINE auto  epsilon() NOEXCEPT { return fp<mpfr::mpreal>::epsilon(); }
-  template < typename T, T_mpreal(T) > static INLINE auto  nan()     NOEXCEPT { return fp<mpfr::mpreal>::nan(); }
-  template < typename T, T_mpreal(T) > static INLINE auto  inf()     NOEXCEPT { return fp<mpfr::mpreal>::inf(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const zero()    NOEXCEPT { return fp<mpfr::mpreal>::zero(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const one()     NOEXCEPT { return fp<mpfr::mpreal>::one(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const two()     NOEXCEPT { return fp<mpfr::mpreal>::two(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const nhalf()   NOEXCEPT { return fp<mpfr::mpreal>::nhalf(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const epsilon() NOEXCEPT { return fp<mpfr::mpreal>::epsilon(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const nan()     NOEXCEPT { return fp<mpfr::mpreal>::nan(); }
+  template < typename T, T_mpreal(T) > static INLINE auto const inf()     NOEXCEPT { return fp<mpfr::mpreal>::inf(); }
 #undef T_mpreal
 
 }
